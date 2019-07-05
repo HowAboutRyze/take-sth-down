3a56e46e8f8f125e2637776f5a8ae2adac67d579
HowAboutRyze <lizhongwei157@163.com>
2019-07-05 22:26:19 +0800

    blog(other):增加利用720云做全景展示;

--- docs/.vuepress/sidebarMap/blogMap.js
+++ docs/.vuepress/sidebarMap/blogMap.js
@@ -2,7 +2,8 @@ const blogMap = [
     { title: '博客', dirname: 'blog/intro' },
     { title: 'javascript', dirname: 'blog/js' },
     { title: 'vue', dirname: 'blog/vue' },
-    { title: 'git', dirname: 'blog/git' }
+    { title: 'git', dirname: 'blog/git' },
+    { title: '其他', dirname: 'blog/other' }
 ]
 
 module.exports = blogMap;
